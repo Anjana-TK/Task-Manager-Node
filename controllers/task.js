1b23216f078b3aef1c58db5cddeb742f1057e693
Anjana-TK <115389489+Anjana-TK@users.noreply.github.com>
2023-05-03 22:13:31 +0530

    final v1
    TODO : Axios

--- controllers/task.js
+++ controllers/task.js
@@ -1,21 +1,73 @@
+const Task =  require('../models/Task')
 
-
-const getAllTasks = (req,res) => {
-    res.send('get all tasks');
+const getAllTasks = async (req,res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(201).json({tasks:tasks})
+    } catch (err) {
+        res.status(500).json({msg:err})
+    }
 }
-const createTask = (req,res) => {
-    res.json(req.body)
+
+
+const createTask = async (req,res) => {
+    try {
+    const task = await Task.create(req.body)
+    res.status(201).json({task})
+    }
+    catch(err){
+        res.status(500).json({msg:err})
+    }
 }    
-const updateTask = (req,res) => {
-    res.send('update Task')
-}
-const getTask = (req,res) => {
-    res.json({id:req.params.id})
-}
-const deleteTask = (req,res) => {
-    res.send('Delete Task')
-}
 
+const getTask = async (req,res) => {
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID})
+        
+
+        if(!task){
+            return res.status(404).json({msg: `Task not found for task ID ${taskID}  `})
+        }
+
+        res.status(200).json({task})
+    } catch (err) {
+        res.status(500).json({msg:err})
+    }
+}
+const deleteTask = async (req,res) => {
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOneAndDelete({_id:taskID})
+        
+
+        if(!task){
+            return res.status(404).json({msg: `Task not found for task ID ${taskID}  `})
+        }
+
+        res.status(200).json({task})
+    } catch (err) {
+        res.status(500).json({msg:err})
+    }
+}
+const updateTask = async (req,res) => {
+    try {
+        const {id:taskID} = req.params
+
+        const task = await Task.findOneAndUpdate({_id:taskID},req.body,{
+            new : true,
+            runValidators : true
+        })
+        
+        if(!task){
+            return res.status(404).json({msg: `Task not found for task ID ${taskID}  `})
+        }
+
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg:err})
+    }
+}
 
 module.exports = {
     getAllTasks,
